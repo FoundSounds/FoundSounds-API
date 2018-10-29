be2b446279cb961774f88f23a6877bd9022df747
David Jensenius <djensenius@gmail.com>
2018-10-29 15:03:11 -0400

    Seed some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+SOUNDS = 10.times.map do
+  {
+    "latitude": Faker::Address.latitude,
+    "longitude": Faker::Address.longitude,
+    "description": Faker::TwinPeaks.quote
+  }
+end
+
+SOUND_USERS = {
+  "david@example.com": SOUNDS[0,4],
+  "banana@example.com": SOUNDS[5,9]
+}
+
+SOUND_USERS.each do |email, sounds|
+  user = User.new(email: email, password: Faker::Internet.password, username: Faker::Internet.username)
+  sounds.each { |sound| user.sounds.build(sound) }
+  user.save
+end
